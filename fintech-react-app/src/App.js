49bf884165158107748dc234720cf1073ec9e8fe
Yogesh Rupnawar <78634372+Yogesh49@users.noreply.github.com>
2021-06-07 14:12:04 +0530

    new react
    new react

---
+++ fintech-react-app/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+import { Home } from './Home';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+
+
+
+function App() {
+
+
+  
+
+
+
+  return (
+   <BrowserRouter>
+
+   <div>
+    
+   </div>
+
+    <div className="container">
+    
+
+      <h3 className="m-3 d-flex justify-content-center">
+       Fintech 
+        
+      </h3>
+     
+
+      <Switch>
+      <Route path ='/' component={Home} exact/>
+         
+      </Switch>
+        </div>
+    </BrowserRouter> 
+  );
+}
+
+export default App;
